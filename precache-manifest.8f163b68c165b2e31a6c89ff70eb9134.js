0953b6645c9cb909c535d40bc0f31c090471e9f6
dq212 <>
2020-01-13 16:27:01 -0500

    Updates

---
+++ precache-manifest.8f163b68c165b2e31a6c89ff70eb9134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102b0e017687b0fc1a3935d24c2a8147",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "5a46b593109acfae80d0",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "374a2b69a33bc5d96f28",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5a46b593109acfae80d0",
+    "url": "/contactmanager/static/js/2.35d45244.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.35d45244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "374a2b69a33bc5d96f28",
+    "url": "/contactmanager/static/js/main.5e167c83.chunk.js"
+  },
+  {
+    "revision": "c9b248b7ef52cebe72fa",
+    "url": "/contactmanager/static/js/runtime-main.bc9037e5.js"
+  }
+]);
